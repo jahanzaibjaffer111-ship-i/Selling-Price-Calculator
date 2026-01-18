b594a3f5d002f2382b0e09e776cbd8c5a4234252
Jahanzaib Jaffer Ali <jahanzaib.jaffer111@gmail.com>
2026-01-18 20:24:34 +0500

    Refactor selling price calculator messages and logic

--- sellingpricecalculator.java
+++ sellingpricecalculator.java
@@ -11,13 +11,13 @@ public class sellingpricecalculator
         try (Scanner read = new Scanner(System.in)) {
             while(true)
             {
-                System.out.println("HELLO");
-                System.out.println("WELCOME TO SELLING PRICE CALCULATOR");
-                System.out.println("ENTRE DISCOUNTED SELLING PRICE");
+                System.out.println("Hello");
+                System.out.println("Welcome to Selling price Calculator");
+                System.out.println("Entre Discounted Selling price");
                 d=read.nextDouble();
                 if (d>0)
                 {
-                    System.out.println("ENTRE DISCOUNT PERCENTAGE");
+                    System.out.println("Entre Discount %");
                     a=read.nextDouble();
                     if(a>0)
                     {
@@ -25,23 +25,35 @@ public class sellingpricecalculator
                         c=b-d;
                         System.out.println("Discount IS");
                         System.out.println(c);
-                        System.out.println("ORIGINAL SELLING PRICE IS");
+                        System.out.println("Original Selling price is");
                         System.out.println(b);
-                        System.out.println("THANKS");
+                        System.out.println("Thank you");
                         System.out.println("Bye");
                         System.out.println(".");
                     }
-                    if(a<0)
+                    else if(a<0)
                     {
-                        System.out.println("DISCOUNT PERCENTAGE CANT BE NEGATIVE");
-                        System.out.println("TRY AGAIN");
+                        System.out.println("Discount % can't be negative");
+                        System.out.println("Try again");
+                        System.out.println(".");
+                    }
+                    else if(a==0)
+                    {
+                        System.out.println("Discount % can't be zero");
+                        System.out.println("Try again");
                         System.out.println(".");
                     }
                 }
-                if (d<0)
+                else if (d<0)
                 {
-                    System.out.println("DISCOUNTED SELLING PRICE CANT BE NEGATIVE");
-                    System.out.println("TRY AGAIN");
+                    System.out.println("Discounted Selling price can't be negative");
+                    System.out.println("Try again");
+                    System.out.println(".");
+                }
+                else if (d==0)
+                {
+                    System.out.println("Discounted Selling price can't be zero");
+                    System.out.println("Try again");
                     System.out.println(".");
                 }
             }
@@ -49,3 +61,4 @@ public class sellingpricecalculator
     }
 
 }
+
